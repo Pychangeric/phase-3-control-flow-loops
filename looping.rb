25f1fd699724089907f7224fc4e282b3e8f40384
pychangeric <pychangeric@gmail.com>
2023-05-15 22:34:39 +0300

    done

--- looping.rb
+++ looping.rb
@@ -1,8 +1,9 @@
 def happy_new_year
-  # your code here
+  (1..10).reverse_each do |num|
+    puts num
+  end
+  puts "Happy New Year!"
 end
-
-# No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
     "FizzBuzz"
@@ -14,11 +15,28 @@ def fizzbuzz(num)
     num
   end
 end
-
 def fizzbuzz_printer
   # your code here
+  (1..100).each do |num|
+    if num % 3 == 0 && num % 5 == 0
+      puts "FizzBuzz"
+    elsif num % 3 == 0
+      puts "Fizz"
+    elsif num % 5 == 0
+      puts "Buzz"
+    else
+      puts num
+    end
+  end
 end
-
 def reverse_string(str)
   # your code here
+  reversed = ""
+  i = str.length - 1
+  while i >= 0
+    reversed += str[i]
+    i -= 1
+  end
+  reversed
+  
 end
